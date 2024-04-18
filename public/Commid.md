f71750d34ec83a14e60b45f1e279862bf2de7d31
Abdumajid-777 <abdumajid951@gmail.com>
2024-04-18 14:16:05 +0500

    Tugadi urra

---
+++ public/Commid.md
@@ -0,0 +1,27 @@
+<!-- <div class="w-[130px] md:w-[150px] lg:w-full mx-auto">
+                    <img class="rounded-2xl select-none border-3 border-black" src="./img/heroSectionImages/A1.png"
+                        alt="">
+                </div>
+                <div class="w-[130px] md:w-[150px] lg:w-full mx-auto">
+                    <img class="rounded-2xl select-none border-3 border-black" src="./img/heroSectionImages/A2.jpg"
+                        alt="">
+                </div>
+                <div class="w-[130px] md:w-[150px] lg:w-full mx-auto">
+                    <img class="rounded-2xl select-none border-3 border-black" src="./img/heroSectionImages/B1.jpg"
+                        alt="">
+                </div>
+                <div class="w-[130px] md:w-[150px] lg:w-full mx-auto">
+                    <img class="rounded-2xl select-none border-3 border-black" src="./img/heroSectionImages/B2.png"
+                        alt="">
+                </div>
+                <div class="w-[130px] md:w-[150px] lg:w-full mx-auto">
+                    <img class="rounded-2xl select-none border-3 border-black" src="./img/heroSectionImages/C1.jpg"
+                        alt="">
+                </div>
+                <div class="w-[130px] md:w-[150px] lg:w-full mx-auto">
+                    <img class="rounded-2xl select-none border-3 border-black" src="./img/heroSectionImages/C2.jpg"
+                        alt="">
+                </div> -->
+                <!-- <a href="#"
+                class="bg-[#e74133] py-2 px-4 rounded-lg text-white text-sm font-semibold flex md:text-xl md:font-black select-none">Eng
+                Questions </a> -->
--- public/js/app.js
+++ public/js/app.js
@@ -1,59 +1,88 @@
-var skip = document.querySelector('#skip')
-var skip1 = document.querySelector('#skip1')
-var score = document.querySelector('#score')
-var totalScore = document.querySelector('#totalScore')
-var countdown = document.querySelector('#countdown')
-var count = 0;
-var scoreCount = 0;
-var duration = 0;
-var qaSet = document.querySelectorAll(".qa_set")
-var qaAnsRow = document.querySelectorAll("#qa_ans_row")
-var box = document.querySelector("#box")
-console.log(box.length);
-skip.addEventListener('click', function () {
-    step();
-    duration = 10;
-})
-qaAnsRow.forEach(function (qaAnsRowSingle) {
-    qaAnsRowSingle.addEventListener('click', function () {
-        setTimeout(function () {
-            step();
-            duration = 10;
-        }, 500)
-        let valid = qaAnsRowSingle.querySelector("input").getAttribute("valid")
-        if (valid) {
-            scoreCount += 10;
-            score.innerHTML = scoreCount;
-            totalScore.innerHTML = scoreCount;
-        } else {
-            scoreCount -= 10;
-            score.innerHTML = scoreCount;
-            totalScore.innerHTML = scoreCount;
-        }
-    })
+let footerBtn = document.querySelector("#footer-button")
+let blueBox = document.querySelector("#blue-box")
+let text = document.querySelector("#text-base")
+
+let dataRandom = quizData.map(function (item) {
+    return quizData[Math.floor(Math.random() * quizData.length)];
 })
 
-function step() {
-    count += 1;
-    for (var i = 0; i < qaSet.length; i++) {
-        qaSet[i].className = 'hidden'
-    }
-    qaSet[count].className = 'qa_set block'
-    if (count == 15) {
-        skip.style.display = 'none'
-        skip1.style.display = 'inline-block'
-        clearInterval(durationTime)
-        countdown.innerHTML = 0;
-    }
+let filterData = dataRandom.filter(function (item, index) {
+    return index < 10
+})
+
+console.log(filterData);
+
+let testNumber = 0
+let testNumber1 = 0
+
+let question = document.querySelector(".question")
+let list = document.querySelector("#box")
+let faq = document.querySelectorAll(".faq")
+let togrijavob = document.querySelector(".testNumber1")
+
+
+function quizANS() {
+    question.innerHTML = filterData[testNumber].question
+    faq[0].innerText = filterData[testNumber].a
+    faq[1].innerText = filterData[testNumber].b
+    faq[2].innerText = filterData[testNumber].c
+    faq[3].innerText = filterData[testNumber].d
 }
 
-var durationTime = setInterval(function () {
-    if (duration == 10) {
-        duration = 0;
-    }
-    duration += 1;
-    countdown.innerHTML = duration;
-    if (countdown.innerHTML == "10") {
-        step()
-    }
-}, 1000)
+quizANS()
+
+
+faq.forEach(function (item, index, arr) {
+    item.addEventListener('click', function () {
+        let nam = null
+        if (item.innerHTML == filterData[testNumber].a) {
+            nam = "a"
+        } else if (item.innerHTML == filterData[testNumber].b) {
+            nam = "b"
+        } else if (item.innerHTML == filterData[testNumber].c) {
+            nam = "c"
+        } else if (item.innerHTML == filterData[testNumber].d) {
+            nam = "d"
+        }
+        if (nam == filterData[testNumber].correct) {
+            testNumber1++
+            togrijavob.innerHTML = testNumber1
+        } else {
+            // alert('notogri')
+        }
+        if (testNumber == 9) {
+            // alert("test tugadi")
+            if (testNumber1 < 5) {
+                list.innerHTML = ` <div class="text-center">
+            <h1 id="text-base" class="text-sm md:text-2xl text-black md:semi-bold capitalize mb-4 md:mb-6 md:flex gap-2 justify-center items-center">Siz <span
+                    class="font-bold color active2">${testNumber}</span> Ta Dan <span class="font-bold color active">${testNumber1}</span> Taga Javob Topdingiz
+            </h1>
+            <p id="text-base" class="text-black md:text-3xl mb-4 text-xl font-bold capitalize">Darajangiz Beginner Emas 🤥</p >
+            <div class="pl-2.5 text-right">
+                <a href="index.html"
+                    class="py-2 px-5 bg-[#0b58a7] text-white rounded-2xl cursor-pointer transition-all hover:bg-[#064484]"
+                    >Bosh Saxifaga</a>
+            </div>
+        </div >
+            `
+            } else {
+                list.innerHTML = ` <div class="text-center">
+            <h1 id="text-base" class="text-sm md:text-2xl text-black md:semi-bold capitalize mb-4 md:mb-6 md:flex gap-2 justify-center items-center">Siz <span
+                    class="font-bold color active2">${testNumber}</span> Ta Dan <span class="font-bold color active">${testNumber1}</span> Taga Javob Topdingiz
+            </h1>
+            <p id="text-base" class="text-black md:text-3xl mb-4 text-xl font-bold capitalize">Darajangiz Beginner 😎</p >
+            <div class="pl-2.5 text-right">
+                <a href="index.html"
+                    class="py-2 px-5 bg-[#0b58a7] text-white rounded-2xl cursor-pointer transition-all hover:bg-[#064484]"
+                    >Bosh Saxifaga</a>
+            </div>
+        </div >
+            `
+            }
+            text.innerHTML = ''
+        } else {
+            testNumber++
+        }
+        quizANS()
+    })
+})
